3f3935754d04eda90ed05d49fc797571f0b299bb
revaldinho <richevans@mac.com>
2021-03-20 10:19:42 +0000

    Add stdout extraction to makefile

---
+++ tools/extract_stdout.py
@@ -0,0 +1,15 @@
+
+# Simple utility to extract STDOUT data from .sim files
+#
+# cat test.sim | python3 extract_stdout.py
+
+import re
+import sys
+
+stdout_re = re.compile("(\s*)?STDOUT\s*:\s*Data\s:\s*0x(?P<hexdata>[0-9a-f]*)\s*\(\s*(?P<decdata>[0-9a-f]*)\).*")
+
+for l in sys.stdin:
+    m = stdout_re.match(l)
+    if m:
+        data = int(m.groupdict()["decdata"])
+        print("%c" % data, end="")
